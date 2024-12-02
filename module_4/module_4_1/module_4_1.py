88580a9f8729a8ec58f407cbfb2f8128870d0341
Андрей <andrepq71q@gmail.com>
2024-12-02 16:40:58 +0300

    module_4_1

--- module_4/module_4_1/module_4_1.py
+++ module_4/module_4_1/module_4_1.py
@@ -1,10 +1,10 @@
-import fake_math as fm
-import true_math as tm
+from fake_math import divide as fm_div
+from true_math import divide as tm_div
 
-result1 = fm.divide(69, 3)
-result2 = fm.divide(3, 0)
-result3 = tm.divide(49, 7)
-result4 = tm.divide(15, 0)
+result1 = fm_div(69, 3)
+result2 = fm_div(3, 0)
+result3 = tm_div(49, 7)
+result4 = tm_div(15, 0)
 
 print(result1)
 print(result2)
